1f1f411496fd7d67bc3200574767b9968aac815d
DmitriyVU <Dmitriy.vladimirovich.u@gmail.com>
2022-12-14 20:38:13 +0300

    Add Project005_IfElse

---
+++ Project005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input your Name:");
+string username = Console.ReadLine();
+
+if(username == "Dima")
+{
+Console.WriteLine("WoW!Hello, Dima");
+}
+else
+{
+    Console.Write("Pff Hello, ");
+    Console.WriteLine(username);
+}
